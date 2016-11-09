449bab6f2998fa83d42e2482e16ae447b120e24b
Aurélien <auron2504@hotmail.fr>
2016-11-09 11:38:48 +0100

    changement affichage des images + select des champions

--- app/models/Champion.java
+++ app/models/Champion.java
@@ -3,6 +3,7 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.Transient;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,4 +12,12 @@ public class Champion extends Model {
     public String nom;
     public String ligne;
     public int preference;
+
+    public String getUrlImage(){
+        switch (nom){
+            case "Leesin":
+                return "//ddragon.leagueoflegends.com/cdn/6.20.1/img/champion/LeeSin.png";
+        }
+        return "//ddragon.leagueoflegends.com/cdn/6.20.1/img/champion/"+nom+".png";
+    }
 }
