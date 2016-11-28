89f2f8c29d970688a78ae603a07c63dcda7d5848
ror4 <aurelien.quetron@outlook.fr>
2016-11-28 16:25:03 +0100

    test jQuery

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,9 +1,34 @@
-$( ".testBoutonJquery" ).click(function() {
-    var texte = $('.testTexteJquery').val();
-    $("#resultText").html(texte);
-});
+function traduitHtml(){
+        var texte = $('.testTexteJquery').val();
+        $("#resultText").html(texte);
+};
 
-$( ".testBoutonJquery2" ).click(function() {
-    var texte = $('#resultText').val();
-    $("#resultText2").html("test"+texte);
-});
+function afficheHtml(){
+        var texte = $('#resultText').html();
+        $("#resultText2").val(texte);
+};
+
+function afficheTt(){
+        var texte = $('#valSelect option:selected').text();
+        $("#resultText3").text(texte);
+        var texte = $('#valSelect').val();
+        $("#resultText4").text(texte);
+};
+
+function afficheTutu(){
+        $('#valSelect').val('u');
+};
+
+function afficheDouble(){
+        var choix = $("input[name='tt']:checked").val();
+        $("input[name='tt2'][value="+choix+"]").prop("checked",true);
+};
+
+function clickAuto(){
+    $(".boutonAuto" ).trigger( "click" );
+};
+
+function compteFils(){
+    var fils = $( ".testDroite" ).find("*").length;
+    $( "span.result" ).text( "There are " + fils + " fils.";
+};
